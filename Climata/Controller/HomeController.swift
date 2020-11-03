bcc11d951be0561e857fac7e3f895aa1e6e4ac7d
gtsofa <jlsnyule@yahoo.com>
2020-11-03 13:22:10 +0300

    initial commit

---
+++ Climata/Controller/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  Climata
+//
+//  Created by Julius on 03/11/2020.
+//  Copyright © 2020 Julius. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Home Screen"
+    }
+}
