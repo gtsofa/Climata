83de9966409c3cca0046ab83abe08840c52daff9
gtsofa <jlsnyule@yahoo.com>
2020-11-03 13:11:23 +0300

    Initial Commit

---
+++ Climata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Climata
+//
+//  Created by Julius on 03/11/2020.
+//  Copyright © 2020 Julius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
