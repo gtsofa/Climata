7cfc46fbfb935abc3941cbdb0d70210783dc0b9a
gtsofa <jlsnyule@yahoo.com>
2020-11-06 01:54:59 +0300

    user can search for a location

--- Climata/New Group/WeatherManager.swift
+++ Climata/New Group/WeatherManager.swift
@@ -7,11 +7,40 @@
 //
 
 import Foundation
+import UIKit
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=62470095f3709929d14ddc5b0e009a0a"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=62470095f3709929d14ddc5b0e009a0a"
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print("url string \(urlString)")
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //create a URL
+        if let url = URL(string: urlString) {
+            //create a URLSession
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print("Response data string:\n \(dataString!)")
+                }
+            }
+            
+            //start the task
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse, error: Error?) {
+        //
     }
 }
